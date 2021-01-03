ba6c8a9d5dd2e7c2a33cfffd605a17009b5bac43
nikhil <parashar.nikhil67@gmail.com>
2021-01-03 22:22:16 +0530

    lec1-4

--- Lec1-1.py
+++ Lec1-1.py
@@ -3,9 +3,12 @@ import cv2 as cv
 import matplotlib.pyplot as plt 
 #%%  Image read & image show
 img = cv.imread('baboon.jpg')
-plt.imshow(img)
+cv.imshow('Baboon.jpg',img)
+cv.waitKey()
 
 # %%   Image write
 cv.imwrite('baboon1.jpg', img)
+cv.destroyAllWindows()
 
 
+# %%
---
+++ Lec1-4.py
@@ -0,0 +1,19 @@
+#%%
+import cv2 as cv 
+import matplotlib.pyplot as plt 
+import numpy as np 
+
+img = cv.imread('baboon.jpg')
+# plt.imshow(img)
+gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+plt.hist(gray.ravel(), 256, [0,256])
+plt.show()
+
+#%% Histogram equalization is the mapping of one distribution to another distribution.
+cv.imshow('Gray',gray)
+img_histogram = cv.equalizeHist(gray)
+cv.imshow('Histogram',img_histogram)
+cv.waitKey() 
+
+# %%
+cv.destroyAllWindows()

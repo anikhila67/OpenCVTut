92944b5945452ffa9488064aec6deebe2bec3d72
nikhil <parashar.nikhil67@gmail.com>
2021-01-11 21:27:40 +0530

    lec2

--- ContourOperations.py
+++ ContourOperations.py
@@ -4,15 +4,19 @@ import numpy as np
 import cv2
 
 # Finding the number of contours in an image
+# crop the contour area
 frame = cv2.imread('Shapes.jpg')
 cv2.imshow('Image', frame)
 cv2.waitKey()
 cv2.destroyAllWindows()
-
 gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 edged = cv2.Canny(gray, 50, 150)
 cnts = cv2.findContours(edged.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 cnts = imutils.grab_contours(cnts) #calculates the center of the contour
 print("Number of contours in the image : ",len(cnts))
-
+cnt = cnts[4]
+cv2.drawContours(frame, [cnt], 0, (0,255,255), 3)
+cv2.imshow('contour draw', frame)
+cv2.waitKey()
+cv2.destroyAllWindows()
 # %%
--- Lec2.py
+++ Lec2.py
@@ -3,32 +3,46 @@ import cv2 as cv
 
 #  Camera read for default camera (0)
 cam = cv.VideoCapture(0)
+
+# width of frame 
+box_size = 234
+width = int(cam.get(3))
 while(1):
     ret, frame = cam.read()
+
+    # Flipped frame 
+    # fframe = cv.flip(frame, 1)
+    # cv.imshow('Flipped frame', fframe)
+    
+    # placing a rectangle on the frame
+    cv.rectangle(frame, (width - box_size, 0), (width, box_size), (0, 250, 150), 2)
+
     gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
 
     # Median Blur
     Mblur = cv.medianBlur(gray,5)
     cv.imshow('Median Blur', Mblur)
 
-    # Gaussian blur
-    Gblur = cv.GaussianBlur(gray,(5,5),0)
-    cv.imshow('Gaussian Blur', Gblur)
+    # # Gaussian blur
+    # Gblur = cv.GaussianBlur(gray,(5,5),0)
+    # cv.imshow('Gaussian Blur', Gblur)
     
-    # Laplacian Blur
-    # check for kernel size ksize= 3, 5, 7, etc
-    Lblur = cv2.Laplacian(frame, cv2.CV_64F, ksize=7)
-    cv.imshow('Laplacian', Lblur)
+    # # Laplacian Blur
+    # # check for kernel size ksize= 3, 5, 7, etc
+    # Lblur = cv2.Laplacian(frame, cv2.CV_64F, ksize=7)
+    # cv.imshow('Laplacian', Lblur)
 
-    # Biletral Blur
-    # change different window size
-    Biletrarblur = cv.bilateralFilter(frame,9,95,95)
-    cv.imshow('Biletral Blur', Biletrarblur)
+    # # Biletral Blur
+    # # change different window size
+    # Biletrarblur = cv.bilateralFilter(frame,9,95,95)
+    # cv.imshow('Biletral Blur', Biletrarblur)
 
     k = cv.waitKey(5) & 0xFF
     if k == 27: 
+        cam.release() 
+        cv.destroyAllWindows()
         break
-
+#%%
 # camera release 
 cam.release() 
 # De-allocate any associated memory usage 
